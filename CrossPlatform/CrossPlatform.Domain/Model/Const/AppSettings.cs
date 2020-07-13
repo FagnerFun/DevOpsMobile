5960e6956f695c7b2c7719fe3ddef8a6f97b976a
Fagner Muniz <cinq.art4@alta-rt.com>
2020-07-13 17:41:03 -0300

    Initial

---
+++ CrossPlatform/CrossPlatform.Domain/Model/Const/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using CrossPlatform.Domain.Model.Enum;
+
+namespace CrossPlatform.Domain.Model.Const
+{
+    public static class AppSettings
+    {
+        public const string ApiUrl = "https://api.themoviedb.org/3";
+        public const string ApiKey = "b6aeac726051a0109930d5d858f66928";
+        public const string ApiImageBaseUrl = "https://image.tmdb.org/t/p/w500/";
+
+        public const string OffLineDataBaseName = "cross.db";
+    }
+}
---
+++ CrossPlatform/CrossPlatform.Infra.SQLite/Repository.cs
@@ -0,0 +1,60 @@
+﻿using CrossPlatform.Domain.Interface.Repository;
+using CrossPlatform.Domain.Interface.Service;
+using CrossPlatform.Domain.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrossPlatform.Infra.SQLite
+{
+    public class Repository<T> : SQLiteConnection, IDisposable, IRepository<T> where T : DBEntity, new()
+    {
+        private readonly IDataBaseAccessService dataBaseAccessService;
+
+        public Repository(IDataBaseAccessService dataBaseAccessService): base(dataBaseAccessService.GetDataBasePath())
+        {
+            this.dataBaseAccessService = dataBaseAccessService;
+
+            CreateTable<T>();
+        }
+
+        public void Add(T entity)
+        {
+            try
+            {
+                BeginTransaction();
+                Insert(entity);
+                Commit();
+            }
+            catch
+            {
+                Rollback();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            try
+            {
+                BeginTransaction();
+                Delete(entity);
+                Commit();
+            }
+            catch
+            {
+                Rollback();
+            }
+        }
+
+        public bool Exist(T Entity)
+        {
+            return (from c in Table<T>() where c.Id == Entity.Id select c).Any();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return (from c in Table<T>() select c);
+        }
+    }
+}
